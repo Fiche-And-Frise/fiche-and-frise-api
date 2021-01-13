f449c6521f3519e798889eac7ebe5ef71ca5de20
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2021-01-13 12:39:51 +0100

    Ajout du controller FriseController

---
+++ src/main/java/com/platine/fiche_frise_api/controller/FriseController.java
@@ -0,0 +1,29 @@
+package com.platine.fiche_frise_api.controller;
+
+import com.platine.fiche_frise_api.bo.Fiche;
+import com.platine.fiche_frise_api.bo.Frise;
+import com.platine.fiche_frise_api.service.FriseService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/frises")
+public class FriseController {
+    private final FriseService friseService;
+
+    public FriseController(FriseService friseService) {
+        this.friseService = friseService;
+    }
+
+    @GetMapping("/")
+    public Iterable<Frise> getAllFrises(){
+        return this.friseService.getAllFrises();
+    }
+
+    @GetMapping("/{id}")
+    public Frise getFrise(@PathVariable int id){
+        return this.friseService.getFrise(id);
+    }
+}
