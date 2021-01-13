d9f12164e5d456099bed1c738dde821e24913e89
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2021-01-13 12:47:07 +0100

    Ajout de la classe FriseServiceImpl

---
+++ src/main/java/com/platine/fiche_frise_api/service/FriseServiceImpl.java
@@ -0,0 +1,38 @@
+package com.platine.fiche_frise_api.service;
+
+import com.platine.fiche_frise_api.bo.Frise;
+import com.platine.fiche_frise_api.repository.FriseRepository;
+
+public class FriseServiceImpl implements FriseService{
+
+   private FriseRepository repository;
+
+   public FriseServiceImpl(FriseRepository repository){
+       this.repository = repository;
+   }
+
+    @Override
+    public Frise getFrise(int id) {
+        return this.repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Iterable<Frise> getAllFrises() {
+        return this.repository.findAll();
+    }
+
+    @Override
+    public Frise createFrise(Frise frise) {
+        return this.repository.save(frise);
+    }
+
+    @Override
+    public Frise updateFrise(Frise frise) {
+        return this.repository.save(frise);
+    }
+
+    @Override
+    public void deleteFrise(int id) {
+        this.repository.delete(this.repository.findById(id).get());
+    }
+}
