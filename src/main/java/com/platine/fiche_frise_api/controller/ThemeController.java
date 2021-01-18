8f3f9def431837b359b0645f575cb0135edf0153
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2021-01-18 16:51:44 +0100

    Ajout du controller ThemeController

---
+++ src/main/java/com/platine/fiche_frise_api/controller/ThemeController.java
@@ -0,0 +1,31 @@
+package com.platine.fiche_frise_api.controller;
+
+import com.platine.fiche_frise_api.bo.Frise;
+import com.platine.fiche_frise_api.bo.Theme;
+import com.platine.fiche_frise_api.service.ThemeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/themes")
+public class ThemeController {
+
+    private ThemeService themeService;
+
+    public ThemeController(ThemeService themeService){
+        this.themeService = themeService;
+    }
+
+    @GetMapping("/")
+    public Iterable<Theme> getAllThemes(){
+        return this.themeService.getAllThemes();
+    }
+
+    @GetMapping("/{id}")
+    public Theme getTheme(@PathVariable int id){
+        return this.themeService.getTheme(id);
+    }
+
+}
