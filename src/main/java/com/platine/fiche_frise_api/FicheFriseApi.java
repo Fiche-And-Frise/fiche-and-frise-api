839b95136748fe975bb1bbcf84cc9d2f4b425b5d
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2021-01-18 17:59:25 +0100

    Modification de la main class

--- src/main/java/com/platine/fiche_frise_api/FicheFriseApi.java
+++ src/main/java/com/platine/fiche_frise_api/FicheFriseApi.java
@@ -2,15 +2,19 @@ package com.platine.fiche_frise_api;
 
 import com.platine.fiche_frise_api.bo.Fiche;
 import com.platine.fiche_frise_api.bo.Frise;
+import com.platine.fiche_frise_api.bo.Theme;
 import com.platine.fiche_frise_api.repository.FicheRepository;
 import com.platine.fiche_frise_api.repository.FriseRepository;
+import com.platine.fiche_frise_api.repository.ThemeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication
 public class FicheFriseApi {
@@ -21,30 +25,49 @@ public class FicheFriseApi {
 
     @Bean
     @Autowired
-    public CommandLineRunner demo(FicheRepository ficherepository, FriseRepository friserepository) {
+    public CommandLineRunner demo(FicheRepository ficheRepository, FriseRepository friseRepository, ThemeRepository themeRepository) {
         return (args) -> {
+
+            List<Fiche> listFiches = new ArrayList<>();
+            List<Frise> listFrises = new ArrayList<>();
+
+
             var ficheMaxime = new Fiche(1, "Première fiche de Maxime",
                     "La première fiche de Maxime",
-                    "19/07/1998",
-                    "Le thème");
+                    "19/07/1998");
 
             var ficheYanis = new Fiche(2, "Première fiche de Yanis",
                     "La première fiche de Yanis, ici c'est le verso",
-                    "Date inconnue",
-                    "L'autre thème");
+                    "Date inconnue");
 
             var friseTest = new Frise(1,"Frise de test",
-                    "Sans Thème",
                     1900,
                     2000,
-                    "Pas d'evenements");
+                    "Pas d'evenements"
+                    );
+
+            listFiches.add(ficheMaxime);
+            listFiches.add(ficheYanis);
+
+            listFrises.add(friseTest);
+
+            var themeTest = new Theme(1,
+                    "TestTheme",
+                    "black",
+                    listFiches,
+                    listFrises);
+
 
             // save a couple of fiches
-            ficherepository.save(ficheMaxime);
-            ficherepository.save(ficheYanis);
+            ficheRepository.save(ficheMaxime);
+            ficheRepository.save(ficheYanis);
 
             //save a frise
-            friserepository.save(friseTest);
+            friseRepository.save(friseTest);
+
+            //save a theme
+            themeRepository.save(themeTest);
+
         };
     }
 
