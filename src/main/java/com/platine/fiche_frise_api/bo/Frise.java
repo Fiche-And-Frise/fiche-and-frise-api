f77f8cef23b84c026544b90dc5a695724acdf23c
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2021-01-18 16:09:56 +0100

    Ajout de l'entity Theme

--- src/main/java/com/platine/fiche_frise_api/bo/Frise.java
+++ src/main/java/com/platine/fiche_frise_api/bo/Frise.java
@@ -3,7 +3,6 @@ package com.platine.fiche_frise_api.bo;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.util.Date;
 
 @Entity
 public class Frise {
---
+++ src/main/java/com/platine/fiche_frise_api/bo/Theme.java
@@ -0,0 +1,63 @@
+package com.platine.fiche_frise_api.bo;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Theme {
+
+    @Id
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String color;
+
+    @OneToMany
+    private List<Fiche> listFiches;
+
+    @OneToMany
+    private List<Frise> listFrises;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public List<Fiche> getListFiches() {
+        return listFiches;
+    }
+
+    public void setListFiches(List<Fiche> listFiches) {
+        this.listFiches = listFiches;
+    }
+
+    public List<Frise> getListFrises() {
+        return listFrises;
+    }
+
+    public void setListFrises(List<Frise> listFrises) {
+        this.listFrises = listFrises;
+    }
+}
